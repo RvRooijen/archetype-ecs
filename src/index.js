3895f4676ed6c4297deebce1728066f7a7210447
rvrooijen <rick.van.rooijen@hotmail.com>
2026-02-06 22:24:20 +0100

    Trim package to pure ECS: remove EventBus, Container, GameLoop
    These modules are not ECS-specific and belong in the consuming
    application. Only EntityManager and Profiler remain.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 export { createEntityManager } from './EntityManager.js';
-export { createEventBus } from './EventBus.js';
-export { createContainer } from './Container.js';
-export { createGameLoop } from './GameLoop.js';
 export { profiler } from './Profiler.js';
